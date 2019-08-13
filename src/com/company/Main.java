798923843d386e21c9ac435dfc8fa4741fddeb72
Maxbum <trunarbum@gmail.com>
2019-08-13 12:42:41 +0300

    Commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,37 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello, what is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello " + name);
+        System.out.println(name + ", enter your password");
 
-        int [] a ={1,2,3,4,5};
-        System.out.println(Arrays.toString(a));
-
+        Scanner scanner2 = new Scanner(System.in);
+        String password1 = scanner2.next();
+        String password2 = "12345";
+        if (password1.equals(password2))
+            System.out.println("Пароль верный");
+        else System.out.println("Пароль не верный");
     }
-}
+
+ /*   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите последовательность цыфр: ");
+        int value = scanner.nextInt();
+        int mirror = 0;
+        while (value != 0) {
+            mirror = mirror * 10 + (value % 10);
+            value = value / 10;
+        }
+        System.out.print("Обраная последовательность цыфр: " + mirror);
+    }*/
+
+ }
+
+
---
+++ src/com/company/Main2.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        for (int i = args.length - 1; i >= 0; i--)
+            System.out.println("Reverse : " + args[i]);
+
+        int[] a = {1, 2, 3, 4, 5};
+        for (int q : a) {
+            System.out.print(q);
+            System.out.println(q);
+
+        }
+    }
+}
+
